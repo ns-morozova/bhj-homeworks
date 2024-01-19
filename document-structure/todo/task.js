fb12b90d1fb53ad8d76bf565e0e6a0189cfb408d
Морозова Наталия <ns_morozova@mail.ru>
2024-01-19 09:00:54 +0300

    Задание «Простой список дел»

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,37 +2,24 @@ const btnTaskAdd = document.getElementById('tasks__add');
 const taskInput = document.getElementById('task__input');
 const tasksList = document.getElementById('tasks__list');
 
-function removeTask (event) {
-    if (!event.target.classList.contains ('task__remove')) {
-        return;
-    }
-    tasksList.removeChild(event.currentTarget);
-}
-
-let counter = 1;
-
 btnTaskAdd.addEventListener('click', (event) => {
-    const task = document.createElement('div');
-    task.className = 'task';
-    task.setAttribute('data-id', counter.toString());
-
-    const taskTitle = document.createElement('div');
-    taskTitle.className = 'task__title';
-    task.appendChild(taskTitle);
-
-    const remove = document.createElement('a');
-    remove.className = 'task__remove';
-    remove.innerHTML = '<a href="#" class="task__remove">&times;</a>';
-    remove.setAttribute('data-id', counter.toString());
-    task.appendChild(remove);
-
-    task.addEventListener('click', removeTask);
-
-    
-    taskTitle.innerHTML = taskInput.value;
-    tasksList.appendChild(task);
-    counter ++;
-    taskInput.value = '';
-
     event.preventDefault();
+
+    tasksList.insertAdjacentHTML('afterbegin', `
+        <div class="task">
+            <div class="task__title">
+                ${taskInput.value}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
+        </div>
+    `);
+
+    const remove = tasksList.querySelector('.task').querySelector('.task__remove');
+
+    remove.addEventListener('click', (event) => {
+        tasksList.removeChild(event.currentTarget.parentElement);
+        let a = 1;
+    });
+
+    taskInput.value = '';
 })
